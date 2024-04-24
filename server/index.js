c75cfa0e102e821f3c26542bd7034052a7cf7649
Mihir <mihirkantho@gmail.com>
2024-04-24 16:44:59 +0600

    final touch

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = 3000;
+const PORT = 3030;
 app.listen(PORT, (err) => {
   if (err) console.error('❌ Unable to connect to the server: ', err);
   console.log(`🌍 Server listening on port ${PORT}`);
